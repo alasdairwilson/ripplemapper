3bcf3c3ecbc36252393ef96fc7654e47c831a4f9
Alasdair Wilson <alasdair.wlsn@gmail.com>
2025-04-23 10:51:52 +0100

    fix rejected project changes

--- ripplemapper/__init__.py
+++ ripplemapper/__init__.py
@@ -1,1 +1,8 @@
 from ripplemapper.version import __version__  # noqa: F401
+from . import analyse
+from . import classes
+from . import contour
+from . import image
+from . import io
+from . import visualisation
+from . import data
